516e6f58a8815a497bbc58fcb2b5da49e299f6af
Anthony Gitter <gitter@biostat.wisc.edu>
2015-12-21 07:39:19 -0600

    Remove unused sort_table function

--- PCSF.py
+++ PCSF.py
@@ -6,16 +6,6 @@ import operator
 import subprocess
 from optparse import OptionParser
 
-def sort_table(table, cols):
-    """ sort a table by multiple columns
-        table: a list of lists (or tuple of tuples) where each inner list 
-               represents a row
-        cols:  a list (or tuple) specifying the column numbers to sort by
-               e.g. (1,0) would sort by column 1, then by column 0
-    """
-    for col in reversed(cols):
-        table = sorted(table, key=operator.itemgetter(col))
-    return table
 
 # Return a list of all unique receptors in the file
 def givenset(stppath,targetfile):
